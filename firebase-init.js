5231c7c5886051b5faf6864772c24dee550a0d56
AnnsuChoudhary <annsuchoudhary2004@gmail.com>
2025-08-26 23:31:58 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -2,16 +2,11 @@
 
 // Your Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAI7XhQbo08cEP_YFtmmjr7Z4Bj50bRaMg",
-  authDomain: "starlab-adventure.firebaseapp.com",
-  projectId: "starlab-adventure",
-  storageBucket: "starlab-adventure.firebasestorage.app",
-  messagingSenderId: "550598361567",
-  appId: "1:550598361567:web:2a99e97d63ad3406aec46a",
-  measurementId: "G-5P05SX2MKM"
+  //
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.auth();
 firebase.firestore();
+
