a55d241508514145599caa880d3920a04ff59e0c
AnnsuChoudhary <annsuchoudhary2004@gmail.com>
2025-08-26 23:35:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,13 +24,7 @@ import { getFirestore, collection, getDocs } from "https://www.gstatic.com/fireb
 
 // Your Firebase config (replace with actual)
 const firebaseConfig = {
-  apiKey: "AIzaSyAI7XhQbo08cEP_YFtmmjr7Z4Bj50bRaMg",
-  authDomain: "starlab-adventure.firebaseapp.com",
-  projectId: "starlab-adventure",
-  storageBucket: "starlab-adventure.firebasestorage.app",
-  messagingSenderId: "550598361567",
-  appId: "1:550598361567:web:2a99e97d63ad3406aec46a",
-  measurementId: "G-5P05SX2MKM"
+  //
 };
 
 const app = initializeApp(firebaseConfig);
@@ -95,3 +89,4 @@ const chatbotBox = document.getElementById('chatbot-box');
 chatIcon.addEventListener('click', () => {
     chatbotBox.style.display = chatbotBox.style.display === 'none' ? 'flex' : 'none';
 });
+
