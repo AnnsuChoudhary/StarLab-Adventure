f612c92abf1af0a5a1d7147aa69eb468cb814747
AnnsuChoudhary <annsuchoudhary2004@gmail.com>
2025-08-26 23:35:08 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -36,13 +36,7 @@ import {
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyAI7XhQbo08cEP_YFtmmjr7Z4Bj50bRaMg",
-  authDomain: "starlab-adventure.firebaseapp.com",
-  projectId: "starlab-adventure",
-  storageBucket: "starlab-adventure.firebasestorage.app",
-  messagingSenderId: "550598361567",
-  appId: "1:550598361567:web:2a99e97d63ad3406aec46a",
-  measurementId: "G-5P05SX2MKM"
+  //
 };
 
 const app = initializeApp(firebaseConfig);
@@ -149,5 +143,6 @@ logoutBtn.addEventListener("click", () => {
     window.location.href = "login.html";
   });
 });
+
 
 
