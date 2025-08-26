4cb3c4c4c07fa03e716088cc35d0f5813f6a55a7
AnnsuChoudhary <annsuchoudhary2004@gmail.com>
2025-08-26 23:27:57 +0530

    Update login.js

--- login.js
+++ login.js
@@ -18,14 +18,7 @@ import {
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyAI7XhQbo08cEP_YFtmmjr7Z4Bj50bRaMg",
-  authDomain: "starlab-adventure.firebaseapp.com",
-  projectId: "starlab-adventure",
-  storageBucket: "starlab-adventure.firebasestorage.app",
-  messagingSenderId: "550598361567",
-  appId: "1:550598361567:web:2a99e97d63ad3406aec46a",
-  measurementId: "G-5P05SX2MKM"
-};
+  //
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -122,6 +115,7 @@ resetPasswordBtn.addEventListener("click", () => {
       errorMsg.textContent = "Error: " + error.message;
     });
 });
+
 
 
 
