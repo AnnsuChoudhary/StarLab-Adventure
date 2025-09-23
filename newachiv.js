aeb3ef4856d2eef640a75b30bbcae536df7ea726
AnnsuChoudhary <annsuchoudhary2004@gmail.com>
2025-09-23 12:29:59 +0530

    Update newachiv.js

--- newachiv.js
+++ newachiv.js
@@ -1,12 +1,6 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAI7XhQbo08cEP_YFtmmjr7Z4Bj50bRaMg",
-  authDomain: "starlab-adventure.firebaseapp.com",
-  projectId: "starlab-adventure",
-  storageBucket: "starlab-adventure.appspot.com",
-  messagingSenderId: "550598361567",
-  appId: "1:550598361567:web:2a99e97d63ad3406aec46a",
-  measurementId: "G-5P05SX2MKM"
+  
 };
 
 // Initialize Firebase
@@ -196,3 +190,4 @@ function displayStreak(count) {
   document.getElementById("streakDisplay").innerText = `🔥 Daily Streak: ${count} days`;
   displayBadges(count);
 }
+
